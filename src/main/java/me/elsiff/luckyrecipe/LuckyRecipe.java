c1d5a5073d8dba06372b2be44c2af0b8243e0183
elsiff <elsiff1002@gmail.com>
2016-05-30 23:09:23 +0900

    Add the reloading command

--- src/main/java/me/elsiff/luckyrecipe/LuckyRecipe.java
+++ src/main/java/me/elsiff/luckyrecipe/LuckyRecipe.java
@@ -1,5 +1,7 @@
 package me.elsiff.luckyrecipe;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,5 +25,16 @@ public class LuckyRecipe extends JavaPlugin {
 
     public RecipeManager getRecipeManager() {
         return recipeManager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("luckyrecipe") && sender.isOp()) {
+            getRecipeManager().loadRecipeList();
+
+            sender.sendMessage("[LuckyRecipe] Successfully reloaded the configuration files!");
+        }
+
+        return true;
     }
 }
